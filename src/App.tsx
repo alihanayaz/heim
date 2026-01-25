049779fd744e9c50341d90a49126038f65b1e187
alihanayaz <alihan@alihanayaz.com>
2026-01-25 15:16:46 +0300

    feat: add Calendar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Header } from "@/features/header";
 import { SearchBar } from "@/features/search";
+import { Calendar } from "@/features/calendar";
 
 export default function App() {
   return (
     <div className="mx-auto flex min-h-dvh w-full max-w-3xl flex-col px-4 py-24 md:px-2">
       <Header />
       <SearchBar />
+      <Calendar />
     </div>
   );
 }
---
+++ src/features/calendar/calendar.tsx
@@ -0,0 +1,84 @@
+import { useCalendar } from "./use-calendar";
+import { Button } from "@/components";
+import { format, isSameMonth, isToday } from "date-fns";
+import { ChevronLeftIcon, ChevronRightIcon, RotateCcwIcon } from "lucide-react";
+
+export default function Calendar() {
+  const { calendar, handleCalendar } = useCalendar();
+
+  return (
+    <div className="mx-auto w-full max-w-xl select-none">
+      <hr className="my-12 w-12" />
+
+      <div className="mb-8 flex items-center justify-between px-4">
+        <div className="flex flex-col gap-1">
+          <h3 className="text-3xl font-bold tracking-tight">
+            {format(calendar.currentDate, "MMMM yyyy")}
+          </h3>
+          <p className="text-foreground-subtle text-xs tracking-widest uppercase">
+            Calendar
+          </p>
+        </div>
+
+        <div className="flex items-center gap-2">
+          {!isSameMonth(calendar.currentDate, calendar.initialDate) && (
+            <Button onClick={handleCalendar.reset} variant="ghost" size="icon">
+              <RotateCcwIcon className="size-5" />
+            </Button>
+          )}
+          <div className="bg-border/20 flex rounded-full p-1">
+            <Button
+              onClick={handleCalendar.prevMonth}
+              variant="ghost"
+              size="icon"
+              className="rounded-full"
+            >
+              <ChevronLeftIcon className="size-6" />
+            </Button>
+            <Button
+              onClick={handleCalendar.nextMonth}
+              variant="ghost"
+              size="icon"
+              className="rounded-full"
+            >
+              <ChevronRightIcon className="size-6" />
+            </Button>
+          </div>
+        </div>
+      </div>
+
+      <div className="border-border/20 mb-4 grid grid-cols-7 place-items-center border-b pb-4">
+        {["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"].map((d) => (
+          <div
+            className="text-foreground-subtle text-xs font-semibold tracking-widest uppercase"
+            key={d}
+          >
+            {d}
+          </div>
+        ))}
+      </div>
+
+      <div className="grid grid-cols-7 gap-4">
+        {calendar.days.map((day) => {
+          const currentMonth = isSameMonth(day, calendar.currentDate);
+          const today = isToday(day);
+
+          return (
+            <div
+              key={day.toISOString()}
+              className="flex items-center justify-center"
+            >
+              <Button
+                variant={today ? "active" : "ghost"}
+                className="size-12 text-lg"
+                disabled={!currentMonth}
+              >
+                {day.getDate()}
+              </Button>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
